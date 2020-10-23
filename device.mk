121e3908c6c21a1095251410ebb1dea76a03b333
Jarl-Penguin <jarlpenguin@outlook.com>
2020-10-24 00:23:07 +0300

    hannah: Decommonize hals.conf
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I41cedfe3c8f2f0956218298dac38c761eaf13790

--- device.mk
+++ device.mk
@@ -59,6 +59,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev_ahannah.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev_ahannah.conf
 

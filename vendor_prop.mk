bd222d3cf13d72e9eaed1d432e76dc07de3f35a8
menorziin <71452169+menorziin@users.noreply.github.com>
2020-12-05 22:17:00 -0300

    jeter: fix Playstore

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -71,6 +71,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.cabl=0 \
     ro.sf.lcd_density=320 \
+    ro.opengles.version=196610 \
     vendor.display.enable_default_color_mode=1
 
 # Fingerprint
